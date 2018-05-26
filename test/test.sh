7fb09a5a1da330977fb3988f0b9eecbc3207e0cc
Nicolas VINCENT <nico.vince@gmail.com>
2018-05-26 10:50:00 +0200

    unit test: complex type and ctype

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-YAML_FILES="ctypes.yaml ctypes_array.yaml ctypes_and_ctypes_array.yaml complex_type.yaml"
+YAML_FILES="ctypes.yaml ctypes_array.yaml ctypes_and_ctypes_array.yaml complex_type.yaml complex_and_ctype.yaml"
 
 for y in ${YAML_FILES}; do
   ../genmsg.py ${y} --py-gen
